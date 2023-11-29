6f483924e9a3505a55232ad209bd433ec8813048
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2023-11-29 13:39:30 +0530

    Time: 4 ms (30.61%) | Memory: 6.2 MB (94.23%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
+        // return __builtin_popcount(n);
+        int c=0;
+        while(n!=0)
+        {
+            c+=n&1;
+            n=n>>1;
+                    }
+        return c;
     }
 };
