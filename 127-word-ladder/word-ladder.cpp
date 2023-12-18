457df3624a96f07dccd48e19d43ffcac652cedf4
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2023-12-18 18:08:13 +0530

    Time: 1812 ms (8.26%) | Memory: 35.1 MB (17.06%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -3,11 +3,18 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         queue<pair<string,int>> q;
         q.push({beginWord,1});
+        // unordered_map<string,int> vis;
+        // for(int i=0;i<wordList.size();i++)
+        // {
+        //     vis[wordList[i]]=0;
+        // }
+        // int ans=INT_MAX;
         unordered_set<string> st(wordList.begin(),wordList.end());
         while(!q.empty())
         {
             string temp=q.front().first;
             int l=q.front().second;
+            // vis[temp]=1;
             st.erase(temp);
             q.pop();
             if(temp==endWord)
@@ -19,12 +26,15 @@ public:
                 {
                     if(temp[i]==j)continue;
                     temp[i]=j;
-                    if(st.find(temp)!=st.end())
+                    // cout<<temp[i]<<"  "<<temp<<"   ";
+                    if(st.find(temp)!=st.end() /*&& vis[temp]!=1*/)
                     {
+                        // cout<<temp<<"  ";
                         q.push({temp,l+1});
                     }
+                    temp=h;
                 }
-                temp=h;
+                // cout<<endl;
             }
         }
         return 0;
