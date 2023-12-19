09be995742c439e0390c61b7f6bd10852b520f62
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2023-12-20 01:09:30 +0530

    Time: 19 ms (67.20%) | Memory: 13.6 MB (98.61%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -21,6 +21,19 @@ public:
         }
         return true;
     }
+    bool solvedfs(int node,int col,vector<int>&color,vector<vector<int>>&graph)
+    {
+        color[node]=col;
+        for(auto it:graph[node])
+        {
+            if(color[it]==-1)
+            {
+                if(solvedfs(it,!col,color,graph)==false)return false;
+            }
+            else if(color[it]==col)return false;
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         vector<int> color(graph.size());
         for(int i=0;i<graph.size();i++)
@@ -28,7 +41,8 @@ public:
         for(int i=0;i<graph.size();i++)
         {
             if(color[i]==-1)
-            if(solve(i,color,graph)==false)return false;
+            // if(solve(i,color,graph)==false)return false;
+            if(solvedfs(i,0,color,graph)==false)return false;
         }      
         return true;
     }
