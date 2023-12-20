547acd1751818224a945208f0702614562b91cb6
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2023-12-20 15:33:23 +0530

    Time: 17 ms (66.08%) | Memory: 13.3 MB (96.95%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& p) {
+        queue<int> q;
+        vector<int> adj[n];
+        for(int i=0;i<p.size();i++)
+        {
+            adj[p[i][0]].push_back(p[i][1]);
+        }
+        int cnt=0;
+        vector<int> indegree(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(auto it:adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        vector<int> vis(n,0);
+        while(!q.empty())
+        {
+            int x=q.front();
+            q.pop();
+            cnt++;
+            vis[x]=1;
+            for(auto it:adj[x])
+            {
+                if(!vis[it])
+                {
+                    indegree[it]--;
+                    if(indegree[it]==0)q.push(it);
+                }
+            }
+        }
+        return (cnt==n);
+    }
+};
