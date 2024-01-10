ccd5440a741ef1759ff7433dd62acf3dcaa3434e
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-01-11 01:23:19 +0530

    Time: 704 ms (12.92%) | Memory: 161.1 MB (38.50%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -6,91 +6,46 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    // void nodeCount(TreeNode*root,int &count,unordered_map<int,bool> &mp)
-    // {
-    //     if(!root && !root->left && !root->right)return ;
-    //     if(root->left) {
-    //     count++;
-    //     mp[root->left->val]=false;
-    //     nodeCount(root->left,count,mp);
-    //     }
-    //     if(root->right){
-    //         mp[root->right->val]=false;
-    //     count++;    
-    //     nodeCount(root->right,count,mp);
-    //     } 
-
-    // }
     int amountOfTime(TreeNode* root, int start) {
-        int count=1;
-        // unordered_map<int,bool> mp;
-        // nodeCount(root,count,mp);
-        // mp[root->val]=false;
-        // cout<<count;
-        // for(auto it:mp)
-        // {
-        //     cout<<it.first<<" "<<it.second<<endl;
-        // }
-        unordered_map<int,set<int>>adj;
-        // TreeNode*temp=root;
+        unordered_map<int, vector<int>> adj;
         queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty())
-        {
-            auto temp=q.front();
+        while (!q.empty()) {
+            auto temp = q.front();
             q.pop();
-            if(temp->left){
+            if (temp->left) {
                 q.push(temp->left);
-                adj[temp->val].insert(temp->left->val);
-                adj[temp->left->val].insert(temp->val);
+                adj[temp->val].push_back(temp->left->val);
+                adj[temp->left->val].push_back(temp->val);
             }
-            if(temp->right){
+            if (temp->right) {
                 q.push(temp->right);
-                adj[temp->val].insert(temp->right->val);
-                adj[temp->right->val].insert(temp->val);
+                adj[temp->val].push_back(temp->right->val);
+                adj[temp->right->val].push_back(temp->val);
             }
         }
-        // for(auto i:adj){
-        //     for(auto it:i.second)
-        //     {
-        //         cout<<i.first<<"->"<<it<<endl;
-        //     }
-        // }
-        
-        queue<pair<int,pair<int,int>>> qe;
-        qe.push({start,{-1,0}});
-        int ans=0;
-        while(!qe.empty())
-        {
-            
-            int val=qe.front().first;
-            int parent=qe.front().second.first;
-            int level=qe.front().second.second;
-            ans=level;
-            // int size=q.size();
+        queue<pair<int, pair<int, int>>> qe;
+        qe.push({start, {-1, 0}});
+        int ans = 0;
+        while (!qe.empty()) {
+            int val = qe.front().first;
+            int parent = qe.front().second.first;
+            int level = qe.front().second.second;
+            ans = level;
             qe.pop();
-            // for(int i=0;i<size;i++)
-            // {
-                for(auto it:adj[val])
-                {
-                    // if(mp[it]==0)
-                    // {
-                        cout<<it<<"  ->"<<endl;
-                        if(it==parent)continue;
-                        // mp[it]=1;
-                        qe.push({it,{val,level+1}});
-                    // }
-                }
-                // qe.push({})
-            // }
+            for (auto it : adj[val]) {
+                cout << it << "  ->" << endl;
+                if (it == parent)
+                    continue;
+                qe.push({it, {val, level + 1}});
+            }
         }
-
-        return ans;      
-        
+        return ans;
     }
 };
