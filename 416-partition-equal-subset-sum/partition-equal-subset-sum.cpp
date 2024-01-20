644f3a818b6740f49f6f1783f39e3a9ac8fa7f2a
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-01-20 17:41:26 +0530

    Time: 167 ms (83.07%) | Memory: 351.3 MB (6.44%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -10,10 +10,28 @@ public:
         bool nottake=solve(nums,sum,idx+1,dp);
         return dp[idx][sum]=take||nottake;
     }
+    bool tab(vector<int>&nums,int target)
+    {
+        vector<vector<int>> dp(nums.size()+1,vector<int>(target+1,0));
+        for(int i=0;i<=nums.size();i++)dp[i][0]=1;
+        for(int idx=nums.size()-1;idx>=0;idx--)
+        {
+            for(int t=0;t<=target/2;t++)
+            {
+                bool take=false;
+                if(t-nums[idx]>=0)
+                    take=dp[idx+1][t-nums[idx]];
+                bool nottake=dp[idx+1][t];
+                dp[idx][t]=take or nottake;
+            }
+        }
+        return dp[0][target/2];
+    }
     bool canPartition(vector<int>& nums) {
         int sum=accumulate(nums.begin(),nums.end(),0);
         if(sum&1)return false;
         vector<vector<int>> dp(nums.size()+1,vector<int>(sum+1,-1));
-        return solve(nums,sum/2,0,dp);
+        // return solve(nums,sum/2,0,dp);
+        return tab(nums,sum);
     }
 };
