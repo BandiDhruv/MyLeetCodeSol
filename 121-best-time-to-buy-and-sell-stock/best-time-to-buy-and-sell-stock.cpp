c9f093e1f0cd22d4e88ce7de6e2a66de177ff953
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-01-26 14:01:31 +0530

    Time: 104 ms (31.96%) | Memory: 95.7 MB (12.06%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int ans=INT_MIN;
-        vector<int> l(prices.size()),r(prices.size());
-        l[0]=prices[0];
-        r[prices.size()-1]=prices[prices.size()-1];
-        for(int i=1;i<prices.size();i++)l[i]=min(l[i-1],prices[i]);
-        for(int i=prices.size()-2;i>=0;i--)r[i]=max(r[i+1],prices[i]);
-        for(int i=0;i<prices.size()-1;i++)
+        // vector<int> l(prices.size()),r(prices.size());
+        // l[0]=prices[0];
+        // r[prices.size()-1]=prices[prices.size()-1];
+        // for(int i=1;i<prices.size();i++)l[i]=min(l[i-1],prices[i]);
+        // for(int i=prices.size()-2;i>=0;i--)r[i]=max(r[i+1],prices[i]);
+        // for(int i=0;i<prices.size()-1;i++)
+        // {
+        //     ans=max(ans,r[i]-l[i]);
+        // }
+        int minprofit=INT_MAX;
+        for(int i=0;i<prices.size();i++)
         {
-            ans=max(ans,r[i]-l[i]);
+            if(prices[i]<minprofit)
+                minprofit=prices[i];
+            else ans=max(ans,prices[i]-minprofit);
         }
         return ans<0?0:ans;
+
     }
 };
