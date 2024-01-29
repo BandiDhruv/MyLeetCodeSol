ede6ba8ffbfb0e7073c12a74526336c4394a4475
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-01-29 16:46:26 +0530

    Time: 1798 ms (11.39%) | Memory: 17.4 MB (29.08%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -14,8 +14,27 @@ public:
         }
         return dp[n][k]=ans;
     }
+    int tab(int n,int k)
+    {
+        vector<vector<int>> dp(n+1,vector<int>(k+1,0));
+        for(int i=0;i<=n;i++)dp[i][0]=1;
+
+        // int ans=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=k;j++)
+            {
+                for(int inv=0;inv<=min(j,i-1);inv++)
+                {
+                    dp[i][j]=(dp[i][j]+dp[i-1][j-inv])%m;
+                }
+            }
+        }
+        return dp[n][k];
+    }
     int kInversePairs(int n, int k) {
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        return solve(n,k,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        // return solve(n,k,dp);
+        return tab(n,k);
     }
 };
