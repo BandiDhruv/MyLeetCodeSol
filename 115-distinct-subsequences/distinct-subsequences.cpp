c470fb9e605b6a2e7712d645c32c104b1363313d
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-01-30 14:12:30 +0530

    Time: 1752 ms (6.18%) | Memory: 421.3 MB (10.10%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -3,13 +3,30 @@ public:
     int solve(string s,string t,int i,int j,vector<vector<int>>&dp)
     {
         if(i==s.size() && j==t.size()) return 1;
-        if(i>=s.size() || j>=s.size()) return 0;
+        if(i>s.size() || j>s.size()) return 0;
         if(dp[i][j]!=-1)return dp[i][j];
         if(s[i]==t[j]) return dp[i][j]=solve(s,t,i+1,j+1,dp)+solve(s,t,i+1,j,dp);
         return dp[i][j]=solve(s,t,i+1,j,dp);
     }
+
+    // int tab(string &s, string &t) {
+    //     vector<vector<double>> dp(s.size() + 1, vector<double>(t.size() + 1, 0));
+    //     dp[s.size()][t.size()] = 1;
+
+    //     for (int i = s.size() - 1; i >= 0; i--) {
+    //         for (int j = t.size() - 1; j >= 0; j--) {
+    //             if (s[i] == t[j]) dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+    //             else dp[i][j] = dp[i + 1][j];
+    //         }
+    //     }
+
+    //     return dp[0][0];
+    // }
+
+
     int numDistinct(string s, string t) {
         vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,-1));
         return solve(s,t,0,0,dp);
+        // return tab(s,t);
     }
 };
