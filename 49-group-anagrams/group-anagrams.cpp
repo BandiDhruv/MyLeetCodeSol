f0c0af3b02f133bf95429272ed1431e4b466fd85
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-02-06 13:06:54 +0530

    Time: 33 ms (46.51%) | Memory: 27.9 MB (10.89%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -5,9 +5,19 @@ public:
         // vector<string> dup=strs;
         for(int i=0;i<strs.size();i++)
         {
-            string d=strs[i];
+            string d="";
+            vector<int> freq(26,0);
+            for(int j=0;j<strs[i].size();j++)
+                freq[strs[i][j]-'a']++;
+            for(int j=0;j<26;j++)
+            {
+                if(freq[j]>0)
+                {
+                    d+=string(freq[j],j+'a');
+                }
+            }
             // sort(dup[i].begin(),dup[i].end());
-            sort(d.begin(),d.end());
+            // sort(d.begin(),d.end());
             mp[d].push_back(strs[i]);
         }
         vector<vector<string>> ans;
