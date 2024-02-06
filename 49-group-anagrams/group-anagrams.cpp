540b389bf5163081e1dda568697303c7100cddd1
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-02-06 13:03:38 +0530

    Time: 26 ms (79.92%) | Memory: 24.2 MB (28.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> mp;
-        vector<string> dup=strs;
+        // vector<string> dup=strs;
         for(int i=0;i<strs.size();i++)
         {
-            sort(dup[i].begin(),dup[i].end());
-            mp[dup[i]].push_back(strs[i]);
+            string d=strs[i];
+            // sort(dup[i].begin(),dup[i].end());
+            sort(d.begin(),d.end());
+            mp[d].push_back(strs[i]);
         }
         vector<vector<string>> ans;
         for(auto it:mp)
