760d9e9a0cf6e490a270c344634cec7fa453157c
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-02-12 14:45:39 +0530

    Time: 9 ms (42.62%) | Memory: 9 MB (6.68%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int l, int r) {
-        if(l==r)return r;
-        if(l<=1)return 0;
-        int ans=l&r;
-        if((int)log2(l)!=(int)log2(r))
-            return 0;
-        for(int i=l+1;i<r;i++)
-        {
-            ans&=i;
+        if ( l == r ) return l;
+        if ( l <= 1 ) return 0;
+        
+        int b1 = 31 - __builtin_clz(l);
+        int b2 = 31 - __builtin_clz(r);
+        if ( b1 != b2 ) return 0;
+        int ans = (1<<b1);
+        // cout << ans;
+        for(int i = b1; i >= 0 ; i--){
+            int l_bit = 0;
+            int r_bit = 0;
+            if ( (l&(1<<i)) != 0 ) l_bit = 1;
+            if ( (r&(1<<i)) != 0 ) r_bit = 1;
+            cout << (1<<i) << endl;
+            if ( l_bit == 1 && r_bit == 1 && (r-(1<<i) )<l){
+                ans |= ( 1 << i);
+            }
+            
         }
         return ans;
     }
