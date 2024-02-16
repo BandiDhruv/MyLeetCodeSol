ef4ee6fd670f9205a4142aee04ce967a0199c208
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-02-16 23:54:28 +0530

    Time: 4 ms (70.43%) | Memory: 12.1 MB (25.23%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& n1, vector<int>& n2) {
-        vector<int> nextG(n2.size());
+    vector<int> solve(vector<int> &nums2){
         stack<int> st;
-        for(int i=n2.size()-1;i>=0;i--)
+        int n=nums2.size();
+        vector<int> ans(n);
+        ans[n-1]=-1;
+        st.push(nums2[n-1]);
+        for(int i=n-2;i>=0;i--)
         {
-            if(st.empty())
+            while(!st.empty() && st.top()<=nums2[i])
             {
-                st.push(n2[i]);
-                nextG[i]=-1;
+                st.pop();
             }
-            else{
-                if(n2[i]<st.top())
-                {
-                    nextG[i]=st.top();
-                }
-                else{
-                    while(!st.empty() && n2[i]>st.top())
-                    {
-                        st.pop();
-                    }
-                    if(st.empty())
-                        nextG[i]=-1;
-                    else
-                        nextG[i]=st.top();
-                }
-            }
-            st.push(n2[i]);
+            if(st.empty())
+                ans[i]=-1;
+            else ans[i]=st.top();
+            st.push(nums2[i]);
         }
-        vector<int> ans(n1.size());
+        return ans;
+    }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> mp;
-        for(int i=0;i<n2.size();i++){
-            mp[n2[i]]=i;
-        }
-        for(int i=0;i<n1.size();i++)
+        for(int i=0;i<nums2.size();i++) mp[nums2[i]]=i;
+        vector<int> nge=solve(nums2);
+        vector<int> ans(nums1.size());
+        for(int i=0;i<nums1.size();i++)
         {
-            ans[i]=nextG[mp[n1[i]]];   
+            ans[i]=nge[mp[nums1[i]]];
         }
         return ans;
     }
