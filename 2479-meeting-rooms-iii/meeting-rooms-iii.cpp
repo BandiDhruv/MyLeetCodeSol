6bd35c2ab289a58dc45277dcf53b70dba4ed07c7
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-02-19 16:18:46 +0530

    Time: 288 ms (95.59%) | Memory: 100.3 MB (70.08%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
+typedef pair<long long ,int> P;
     int mostBooked(int n, vector<vector<int>>& me) {
         vector<int> count(n,0);
-        vector<long long> lastAvailable(n,0);
         sort(me.begin(),me.end());
+        priority_queue<P,vector<P>,greater<P>> pq;
+        priority_queue<int,vector<int>,greater<int>> avail;
+        for(int i=0;i<n;i++)avail.push(i);
         for(auto &it:me)
         {
             int start=it[0];
             int end=it[1];
             int duration=end-start;
-            bool found=0;
-            long long earlyroomend=LLONG_MAX;
-            int earlyroomidx=0;
-            for(int room=0;room<n;room++)
+            while(!pq.empty() && pq.top().first<=start)
             {
-                if(lastAvailable[room] <= start)
-                {
-                    count[room]++;
-                    lastAvailable[room]=end;
-                    found=true;
-                    break;
-                }
-                if(lastAvailable[room]<earlyroomend)
-                {
-                    earlyroomend=lastAvailable[room];
-                    earlyroomidx=room;
-                }
+                int roomnumber=pq.top().second;
+                pq.pop();
+                avail.push(roomnumber);
             }
-            if(!found)
+            if(!avail.empty())
             {
-                lastAvailable[earlyroomidx]+=duration;
-                count[earlyroomidx]++;
+                int room=avail.top();
+                avail.pop();
+                pq.push({end,room});
+                count[room]++;
             }
+            else{
+                int room=pq.top().second;
+                long long endtime=pq.top().first;
+                pq.pop();
+                pq.push({endtime+duration,room});
+                count[room]++;
+            }
+
         }
         int ans=0,res=-1;
         for(int i=0;i<count.size();i++)
