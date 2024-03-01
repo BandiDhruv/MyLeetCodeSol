fa5a3580ec219025e8c046ca6f66adf75df68edb
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-03-01 16:28:02 +0530

    Time: 10 ms (63.86%) | Memory: 10.8 MB (66.83%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    static bool cmp(pair<char, int>& a, pair<char, int>& b) {
-        return a.second > b.second;
+    static bool cmp(const pair<char,int> &a,const pair<char,int> &b){
+        return a.second>b.second;
     }
-
     string frequencySort(string s) {
-        unordered_map<char, int> mp;
-        for (char c : s) {
-            mp[c]++;
-        }
-
-        vector<pair<char, int>> freqPairs(mp.begin(), mp.end());
-        sort(freqPairs.begin(), freqPairs.end(), cmp);
-
+        unordered_map<char,int> mp;
+        for(auto &i:s)mp[i]++;
+        vector<pair<char,int>> vp(mp.begin(),mp.end());
+        sort(vp.begin(),vp.end(),cmp);
         string ans;
-        for (auto& pair : freqPairs) {
-            ans += string(pair.second, pair.first);
+        for(auto it:vp){
+            int time=it.second;
+            char ch=it.first;
+            for(int i=0;i<time;i++)
+            {
+                ans.push_back(ch);
+            }
         }
-
         return ans;
     }
 };
