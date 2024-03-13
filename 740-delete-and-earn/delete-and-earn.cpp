897af2bec8199e6305e8f7693662a8b9e7cb5968
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-03-13 17:23:25 +0530

    Time: 19 ms (19.14%) | Memory: 19.7 MB (10.62%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
+    int solve(vector<int>&mp,int idx,vector<int>&dp)
+    {
+        if(idx>=mp.size())return 0;
+        if(dp[idx]!=-1)return dp[idx];
+        return dp[idx]=max(solve(mp,idx+1,dp),solve(mp,idx+2,dp)+mp[idx]*idx);
+    }
     int deleteAndEarn(vector<int>& nums) {
-        map<int,int> mp;
-        for(auto it:nums)
-            mp[it]++;
-        vector<int> dp(2e4+1);
-        dp[0]=0;
-        dp[1]=mp[1];
-        for(int i=2;i<=2e4;i++)
-        {
-            dp[i]=max(dp[i-1],dp[i-2]+mp[i]*i);
-        }
-        return dp[2e4];
+        vector<int> freq(10001,0);
+        vector<int> dp(10005,-1);
+        for(int i=0;i<nums.size();i++)freq[nums[i]]++;
+        return solve(freq,0,dp);
     }
 };
