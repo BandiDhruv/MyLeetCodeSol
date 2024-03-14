862d255538abc98f4e523bdaa1cc5a0a4e1f3508
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-03-14 14:05:57 +0530

    Time: 9 ms (62.68%) | Memory: 14.5 MB (45.97%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -3,16 +3,13 @@ public:
 vector<int>vec;
 vector<vector<int>>ans;
     void dfs(int node,vector<vector<int>>&graph){
-        
         vec.push_back(node);
-        for(auto it:graph[node]){
-            
-                dfs(it,graph);
-                if(it==graph.size()-1)
+        if(node==graph.size()-1)
                 ans.push_back(vec);
-                vec.pop_back();
-                
-            }
+        for(auto it:graph[node]){
+                dfs(it,graph);        
+        }
+        vec.pop_back();
         
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
