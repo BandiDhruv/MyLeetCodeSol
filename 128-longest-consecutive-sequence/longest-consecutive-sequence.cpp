65f5d0ed6de9ee3f9c39e52af8abead0472875d6
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-03-28 18:57:19 +0530

    Time: 131 ms (51.19%) | Memory: 77.4 MB (8.20%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,27 +1,54 @@
+class DisjointSet {
+public:
+    vector<int> size,parent;
+    DisjointSet(int n){
+        parent.resize(n + 1);
+        size.resize( n + 1);
+        for(int i=0;i<=n;i++){
+            size[i] = 1;
+            parent[i] = i;
+        }
+    }
+    int findUPar(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void UnionBySize(int u, int v){
+        int ult_u = findUPar(u);
+        int ult_v = findUPar(v);
+        if(ult_u == ult_v){
+            return;
+        }
+        if(size[ult_u] < size[ult_v]){
+            parent[ult_u] = ult_v;
+            size[ult_v] += size[ult_u];
+        }
+        else{
+            parent[ult_v] = ult_u;
+            size[ult_u] += size[ult_v];
+        }
+    }
+    
+};
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         int n=nums.size();
-        if(n==0)return 0;
-        unordered_set<int> st;
-        int longest=1;
-        for(int i=0;i<n;i++)
-        {
-            st.insert(nums[i]);
+        DisjointSet ds(n);
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])!=mp.end())continue;
+            mp[nums[i]]=i;
+            if(mp.find(nums[i]-1)!=mp.end())ds.UnionBySize(i,mp[nums[i]-1]);
+            if(mp.find(nums[i]+1)!=mp.end())ds.UnionBySize(i,mp[nums[i]+1]);
         }
-        for(auto it:st){
-            if(st.find(it-1)==st.end())
-            {
-                int cnt=1;
-                int x=it;
-                while(st.find(x+1)!=st.end())
-                {
-                    x++;
-                    cnt++;
-                }
-                longest=max(longest,cnt);
-            }
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(i==ds.findUPar(i))ans=max(ans,ds.size[i]);
         }
-        return longest;
+        return ans==INT_MIN?0:ans;
     }
 };
