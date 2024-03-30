5f7d1074499f17d05ea5c644b155ee3d03dec300
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-03-30 14:01:01 +0530

    Time: 21 ms (49.04%) | Memory: 10.3 MB (62.18%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n=s.size();
-        if(t.size()>n)return "";
-        unordered_map<char,int> mp;
-        for(auto it:t)
-        {
-            mp[it]++;
-        }
-        
-        int i=0,j=0,ans=INT_MAX,req=t.size(),start=0;
-        while(j<n)
-        {
-            if(mp[s[j]]>0)req--;
-            mp[s[j]]--;
-            while(req==0)
-            {
-                int temp=j-i+1;
-                if(ans>temp){
-                    ans=temp;
-                    start=i;    
-                }
-                mp[s[i]]++;
-                if(mp[s[i]]>0)req++;
-                i++;
+        map<char,int> mp;
+        for(auto it:t)mp[it]++;
+        int start,ans=INT_MAX,left=0,req=t.size();
+        for(int right=0;right<s.size();right++){
+            if(mp[s[right]]>0){
+                req--;
             }
-            j++;
+            mp[s[right]]--;
+            while(req==0){
+                int wsize=right-left+1;
+                if(wsize<ans){
+                    ans=wsize;
+                    start=left;
+                }
+                mp[s[left]]++;
+                if(mp[s[left]]>0)req++;
+                left++;
+            }
+
         }
-        if(ans==INT_MAX)return "";
-        return s.substr(start,ans);
+        return ans==INT_MAX?"":s.substr(start,ans);
     }
 };
