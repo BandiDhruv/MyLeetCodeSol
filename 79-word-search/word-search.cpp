0908a6e2ba68b46f0e7467765cf0d9cc828409fb
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-04-03 15:29:39 +0530

    Time: 602 ms (54.68%) | Memory: 11.9 MB (18.51%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,83 +1,65 @@
-// class Solution {
-// public:
-//     vector<vector<int>> dir{{1,0},{-1,0},{0,-1},{0,1}};
-//     bool find(vector<vector<char>> &board,int i,int j,int idx,string word)
-//     {
-//         if(idx==word.length())
-//             return true;
-//         if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]=='$')
-//             return false;
-//         if(board[i][j]!=word[idx])
-//             return false;
-//             char temp=board[i][j];
-//             board[i][j]='$';
-//         for(auto it:dir)
-//         {
-//             int i_=i+it[0];
-//             int j_=j+it[1];
-//             if(find(board,i_,j_,idx+1,word))return true;
-//         }
-//         board[i][j]=temp;
-//         return false;
-//     }
-
-//     bool exist(vector<vector<char>>& board, string word) {
-//         int m=board.size(),n=board[0].size();
-//             if(m*n<word.length())
-//                 return false;
-//         for(int i=0;i<m;i++)
-//         {
-//             for(int j=0;j<n;j++)
-//             {
-//                 if(board[i][j]==word[0] && find(board,i,j,0,word))
-//                     return true;
-//             }
-//         }
-//         return false;
-
-//     }
-// };
 class Solution {
 public:
-    int l, m, n;
-    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    bool find(vector<vector<char>>& board, int i, int j, string &word, int idx) {
-        if(idx >= l)
-            return true;
-        
-        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx])
-            return false;
-        
-        char temp = board[i][j];
-        board[i][j] = '$';
-        
-        for(auto& dir : directions) {
-            int i_ = i + dir[0];
-            int j_ = j + dir[1];
-            
-            if(find(board, i_, j_, word, idx+1))
-                return true;
-        }
-        
-        board[i][j] = temp;
-        return false;
+    struct TrieNode{
+        unordered_map<char,TrieNode*> mp;
+        bool isEnd;
+    };
+    TrieNode*getNode(){
+        TrieNode*root=new TrieNode();
+        root->isEnd=false;
+        return root;
     }
-    
+    void insert(TrieNode*temp,string word){
+        TrieNode*root=temp;
+        for(int i=0;i<word.size();i++){
+            char ch=word[i];
+            if(root->mp.find(ch)==root->mp.end())
+                root->mp[ch]=getNode();
+            root=root->mp[ch];
+        }
+        root->isEnd=true;
+    }
+    bool isValid(int x,int y,vector<vector<char>>&board){
+        return (x>=0 && y>=0 && x<board.size() && y<board[0].size()); 
+    }
+    int dx[4]={1,-1,0,0};
+    int dy[4]={0,0,1,-1};
+    bool dfs(vector<vector<char>>&board,int i,int j,TrieNode*root,vector<vector<bool>> &vis){
+        TrieNode*temp=root;
+        vis[i][j]=true;
+        for(int k=0;k<4;k++){
+            int x=i+dx[k];
+            int y=j+dy[k];
+            if(isValid(x,y,board) && temp->mp.find(board[x][y])!=temp->mp.end() && !vis[x][y]){
+                if(dfs(board,x,y,temp->mp[board[x][y]],vis))return true;
+            }
+        }
+        vis[i][j]=false;
+        if(temp->isEnd){
+            cout<<"s";
+            for(auto it:temp->mp){
+                if(it.second!=nullptr)return false;
+            }
+        }
+        return temp->isEnd;
+    }
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-        l = word.length();
-        if(m*n < l)
-            return false;
-        
-        for(int i = 0; i<m; i++) {
-            for(int j = 0; j<n; j++) {
-                if(board[i][j] == word[0] && find(board, i, j, word, 0)) {
-                    return true;
+        TrieNode*root=new TrieNode();
+        insert(root,word);
+        // for(auto it:board){
+        //     for(auto i:it){
+        //         cout<<i<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        vector<vector<bool>> vis(board.size(),vector<bool>(board[0].size(),false));
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                if(board[i][j]==word[0] && !vis[i][j]){
+                    if(dfs(board,i,j,root->mp[board[i][j]],vis))return true;
                 }
             }
         }
-        
         return false;
     }
 };
