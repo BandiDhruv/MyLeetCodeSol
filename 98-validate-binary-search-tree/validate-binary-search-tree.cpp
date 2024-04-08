527609aea7000df61c3831255729b36d8a6da8d1
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-04-08 17:18:55 +0530

    Time: 3 ms (97.93%) | Memory: 20.4 MB (7.37%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,26 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool s(TreeNode*root ,long long lmin,long long rmax)
-    {
-        if(!root)return true;
-        if((root->val<=lmin) || (root->val >= rmax))
-            return false;
-        return (s(root->left,lmin,1ll*root->val) and s(root->right,1ll*root->val,rmax));
+    void solve(TreeNode*root,vector<int>&ans){
+        if(!root)return ;
+        if(root->left)solve(root->left,ans);
+        ans.push_back(root->val);
+        if(root->right)solve(root->right,ans);
     }
     bool isValidBST(TreeNode* root) {
-        long long x=1ll*INT_MIN-1,y=1ll*INT_MAX+1;
-        
-        return s(root->left,x,root->val) and s(root->right,root->val,y);
+        vector<int> ans;
+        solve(root,ans);
+        for(int i=0;i<ans.size()-1;i++){
+            if(ans[i]>=ans[i+1])return false;
+        }
+        return true;
     }
 };
