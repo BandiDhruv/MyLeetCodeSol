fce96d36bfe439bf2a23ee26fed013104df95047
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-05-24 20:40:58 +0530

    Time: 295 ms (37.19%) | Memory: 15.5 MB (46.46%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int countPalindromicSubsequence(string s) {
-        unordered_set<char> set;
-        int count=0;
-        for(auto t:s)
-            set.insert(t);
-        for(auto it:set)
-        {
-            char a=it;
-            int left=-1,right=-1;
-            for(int i=0;i<s.size();i++)
-            {
-                if(s[i]==a)
-                {
-                    if(left==-1)
-                        left=i;
-                    right=i;
+    int countPalindromicSubsequence(string s){
+        int ans=0;
+        unordered_set<char> st;
+        for(char c:s)
+            st.insert(c);
+        for(auto it:st){
+            int i=-1,j=0;
+            for(int k=0;k<s.size();k++){
+                if(s[k]==it){
+                    if(i==-1)i=k;
+                    j=k;
                 }
             }
-            unordered_set<char> st;
-            for(int j=left+1;j<=right-1;j++)
-            {
-                st.insert(s[j]);
-            }
-            count+=st.size();
+            unordered_set<char> temp;
+            for(int idx=i+1;idx<j;idx++)temp.insert(s[idx]);
+            ans+=temp.size();
         }
-        return count;
+        return ans;
     }
 };
