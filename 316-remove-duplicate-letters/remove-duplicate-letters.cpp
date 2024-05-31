53c828971a765cbe05a69f2481902c673eaebaf9
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-05-31 18:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (64.23%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<bool>vis(26,false);
-        // vector<char> a;
-        string a="";
-        unordered_map<char,int> mp;
-        for(int i=0;i<s.size();i++)
-        {
-            mp[s[i]]=i;
+        vector<int>freq(26,0),vis(26,0);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']=i;
+            // vis[it-'a']++;
         }
-        for(int i=0;i<s.size();i++)
-        {
-            if(vis[s[i]-'a'])continue;
-            while(!a.empty() &&  a.back()>s[i] && mp[a.back()]>i)
-            {
-                vis[a.back()-'a']=false;
-                a.pop_back();
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(vis[ch-'a']==1)continue;
+            if(st.empty() || ch>st.top()){
+                st.push(ch);
+                vis[ch-'a']=1;
             }
-            a.push_back(s[i]);
-            vis[s[i]-'a']=true;
+            else{
+                while(!st.empty() && st.top()>ch && freq[st.top()-'a']>i){
+                    char c=st.top();
+                    st.pop();
+                    vis[c-'a']=0;
+                }
+                if(vis[ch-'a']==0){
+                    vis[ch-'a']=1;    
+                    st.push(ch);
+                }
+            }
         }
-        // string ans="";
-        // for(auto it:a)
-            // ans+=it;
-        return a;
+        string temp;
+        while(!st.empty()){
+            cout<<st.top()<<" ";
+            temp.push_back(st.top());
+            st.pop();
+        }
+        reverse(temp.begin(),temp.end());
+        return temp;
     }
 };
