faa15c13825fab450dba9942a17cf9e7206b58c5
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-06-06 20:43:23 +0530

    Time: 78 ms (93.78%) | Memory: 53.3 MB (52.78%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
+        int m=1e9+7,l=0,n=nums.size(),r=n-1,ans=0;
+        vector<int> po(n+1);
+        po[0]=1;
         sort(nums.begin(),nums.end());
-        int l=0,r=nums.size()-1,ans=0,m=1e9+7;
-        vector<int> power(nums.size());
-        power[0]=1;
-        for(int i=1;i<nums.size();i++)
-        {
-            power[i]=(power[i-1]*2)%m;
+        for(int i=1;i<=n;i++){
+            po[i]=(po[i-1]*2)%m;
         }
         while(l<=r){
-        if(nums[l]+nums[r]<=target)
-        {
-            ans=(ans%m+power[r-l])%m;
-            l++;
-        }
-        else
-            r--;
+            if(nums[l]+nums[r]<=target)
+            {
+                ans+=po[r-l];
+                ans%=m;
+                l++;
+            }
+            else r--;
         }
         return ans;
-    }
 
+    }
 };
