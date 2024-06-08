621d805c2d23cd44ed1a4d6018a3f2046e9cb63b
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-06-08 12:28:50 +0530

    Time: 246 ms (13.27%) | Memory: 135.2 MB (14.02%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -4,12 +4,12 @@ public:
         map<int,int> mp;
         mp[0]=0;
         int sum=0;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++)
+        {
             sum+=nums[i];
-            if(mp.find(sum%k)==mp.end()){
-                    mp[sum%k]=i+1;
-            }
-            else if(mp[sum%k]<i)return true;
+            if(mp.find(sum%k)==mp.end())
+                mp[sum%k]=i+1;
+            else if(i-mp[sum%k]>0)return true;
         }
         return false;
     }
