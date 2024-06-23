6d80ba13d002ac6f4cf8a3ccc001569bced6c5f5
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-06-23 18:39:13 +0530

    Time: 121 ms (7.93%) | Memory: 69.1 MB (5.36%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
 
-    int solve(vector<int>&arr,int i,int sign){
+    int solve(vector<int>&arr,int i,int sign,vector<vector<int>>& dp){
         if(i>=arr.size()-1)return 0;
+        if(dp[i][sign]!=-1)return dp[i][sign];
         int cnt=0;
         if(sign==0){
             if(arr[i]>arr[i+1]){
-                cnt=1+solve(arr,i+1,1-sign);
+                cnt=1+solve(arr,i+1,1-sign,dp);
             }
         }
         if(sign==1){
             if(arr[i]<arr[i+1]){
-                cnt=1+solve(arr,i+1,1-sign);
+                cnt=1+solve(arr,i+1,1-sign,dp);
             }
         }
-        return cnt;
+        return dp[i][sign]=cnt;
     }
     int maxTurbulenceSize(vector<int>& arr) {
         int ans=INT_MIN;
+        vector<vector<int>> dp(arr.size()+1,vector<int>(3,-1));
         for(int i=0;i<arr.size();i++){
-            ans=max(ans,1+max(solve(arr,i,0),solve(arr,i,1)));
+            ans=max(ans,1+max(solve(arr,i,0,dp),solve(arr,i,1,dp)));
         }
         return ans;
     }
