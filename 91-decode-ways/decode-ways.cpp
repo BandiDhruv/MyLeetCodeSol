bd5f8337ea6b5b3cef9b3a7eaed9e963eac56385
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-07-17 22:37:48 +0530

    Time: 3 ms (44.64%) | Memory: 8.2 MB (51.53%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-
-    int solve(string s,int idx,int n,vector<int>&dp)
-    {
-        if(idx==n)return 1;
-        if(dp[idx]!=-1)return dp[idx];
-        if(s[idx]=='0')return 0;
-        int result=solve(s,idx+1,n,dp);
-        if(idx+1<n && (s[idx]=='1' || s[idx]=='2' && s[idx+1]<='6'))
-            result+=solve(s,idx+2,n,dp);
-        return dp[idx]=result;
+    int t[102];
+    int solve(string &s,int i){
+        if(i>=s.size())
+            return 1;
+        if(t[i]!=-1)return t[i];
+        int option1=0;
+        if(s[i]!='0')
+            option1=solve(s,i+1);
+        int option2=0;
+        int option3=0;
+        // if(i+1<s.size() && (s[i+1]=='0' || s[i+1]=='1' || s[i+1]=='2' || s[i+1]=='4' || s[i+1]=='5' || s[i+1]=='6' || s[i+1]=='3') && s[i]!='0' && (s[i]=='1' || s[i]=='2'))
+        if(i+1<s.size() && (s[i]=='2' && (s[i+1]=='0' || s[i+1]=='1' || s[i+1]=='2' || s[i+1]=='4' || s[i+1]=='5' || s[i+1]=='6' || s[i+1]=='3')))
+            option2=solve(s,i+2);
+        if(i+1<s.size() && s[i]=='1')
+            option3=solve(s,i+2);
+        return t[i]=option1+option2+option3;
     }
     int numDecodings(string s) {
-        vector<int> dp(s.size()+1,-1);
-        return solve(s,0,s.size(),dp);
+        if(s[0]=='0')return 0;
+        memset(t,-1,sizeof(t));
+        return solve(s,0);
     }
 };
