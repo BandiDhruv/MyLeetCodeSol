bf05bddef2939fdaa494e505c088c2c5d5689870
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-07-30 18:36:50 +0530

    Time: 118 ms (20.89%) | Memory: 53.2 MB (14.35%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         vector<int> suf(s.size(),0),pre(s.size(),0);
-        // for(int i=1;i<s.size();i++){
-        //     pre[i]=pre[i-1];
-        //     if(s[i-1]=='b')pre[i]++;
-        // }
         for(int i=s.size()-2;i>=0;i--){
             suf[i]=suf[i+1];
             if(s[i+1]=='a')
@@ -17,9 +13,6 @@ public:
             if(s[i-1]=='b')pre[i]++;
             ans=min(ans,suf[i]+pre[i]);
         }
-        // for(auto it:suf)cout<<it<<" ";
-        // cout<<endl;
-        // for(auto it:pre)cout<<it<<" ";
-        return ans==INT_MAX?0:ans;
+        return ans;
     }
 };
