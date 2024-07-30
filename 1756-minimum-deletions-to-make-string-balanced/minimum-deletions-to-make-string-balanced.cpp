ff8606090e11880eda3e890e9f6c34d4c6ea397b
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-07-30 18:43:26 +0530

    Time: 91 ms (60.29%) | Memory: 23.7 MB (66.99%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        vector<int> suf(s.size(),0),pre(s.size(),0);
-        for(int i=s.size()-2;i>=0;i--){
-            suf[i]=suf[i+1];
-            if(s[i+1]=='a')
-                suf[i]++;
+        // vector<int> suf(s.size(),0),pre(s.size(),0);
+        int acount=0,bcount=0;
+        // for(int i=s.size()-2;i>=0;i--){
+        //     suf[i]=suf[i+1];
+        //     if(s[i+1]=='a')
+        //         suf[i]++;
+        // }
+        // int ans=suf[0]+pre[0];
+        int ans=INT_MAX;
+        for(int i=0;i<s.size();i++){
+            //  pre[i]=pre[i-1];
+            // if(s[i-1]=='b')pre[i]++;
+            // ans=min(ans,suf[i]+pre[i]);
+            if(s[i]=='a')acount++;
         }
-        int ans=suf[0]+pre[0];
-        for(int i=1;i<s.size();i++){
-             pre[i]=pre[i-1];
-            if(s[i-1]=='b')pre[i]++;
-            ans=min(ans,suf[i]+pre[i]);
+        if(s.size()==acount)return 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a')acount--;
+            ans=min(ans,acount+bcount);
+            if(s[i]=='b')bcount++;
+            // else acount--;
         }
         return ans;
     }
