4adbd9bc06bbc43b983ad11cf953dbeaa63a790b
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-07-31 19:15:37 +0530

    Time: 690 ms (8.03%) | Memory: 284.4 MB (14.09%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    // mergesort()
-    void merge(vector<int>&nums,int i,int j){
-        int mid=i+(j-i)/2;
-        merge(nums,i,mid);
-        merge(nums,mid+1,j);
-        // mergesort(nums,i,j);
+    void mergesort(vector<int>&nums,int l,int mid,int h){
+        int s=l,e=mid+1;
+        vector<int> temp;
+        while(s<=mid && e<=h){
+            if(nums[s]>nums[e])
+                temp.push_back(nums[e++]);
+            else
+                temp.push_back(nums[s++]);
+        }
+        while(s<=mid)temp.push_back(nums[s++]);
+        while(e<=h)temp.push_back(nums[e++]);
+        for (int i=l; i<=h;i++) {
+            nums[i]=temp[i-l];
+        }
+        return;
+    }
+    void merge(vector<int>&nums,int l,int h){
+        if(l>=h)return;
+        int mid=(l+h)/2;
+        merge(nums,l,mid);
+        merge(nums,mid+1,h);
+        mergesort(nums,l,mid,h);
     }
     vector<int> sortArray(vector<int>& nums) {
-        map<int,int> mp;
-        for(auto it:nums)mp[it]++;
-        vector<int> ans;
-        for(auto it:mp){
-            for(int i=0;i<it.second;i++){
-                ans.push_back(it.first);
-            }
-        }
-        return ans;
-        // merge(nums,0,nums.size()-1);
+        merge(nums,0,nums.size()-1);
+        return nums;
     }
 };
