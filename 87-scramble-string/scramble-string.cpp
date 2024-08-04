86f70781481198fee439a9e63566d897b7be0c9e
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-08-04 16:09:02 +0530

    Time: 274 ms (6.62%) | Memory: 35.7 MB (39.74%) - LeetSync

--- 87-scramble-string/scramble-string.cpp
+++ 87-scramble-string/scramble-string.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    map<pair<string,string>,bool>mp;
-    bool solve(string s1,string s2){
-        if(s1 == s2)
-            return true;
-        if(s1.size()!=s2.size())
-            return false;
-        if(mp.find({s1,s2})!=mp.end())return mp[{s1,s2}];
-        int n=s1.size();
-        
-        for(int i=1;i<n;i++){
-            bool swapped=solve(s1.substr(0,i),s2.substr(n-i,i)) and solve(s1.substr(i,n-i),s2.substr(0,n-i));
-            if(swapped)
-                return mp[{s1,s2}]=true;
-            bool notswapped=solve(s1.substr(i,n-i),s2.substr(i,n-i)) and solve(s1.substr(0,i),s2.substr(0,i));
-            if(notswapped)
-                return mp[{s1,s2}]=true;
-        }
-        return mp[{s1,s2}]=false;
-        
-    }
+    map<string,bool> mp;
     bool isScramble(string s1, string s2) {
-        return solve(s1,s2);
+        if(s1.size() != s2.size())
+            return false;
+        if(s1==s2)
+            return true;
+        string key=s1+"_"+s2;
+        bool flag=false;
+        if(mp.find(key)!=mp.end())
+            return mp[key];
+        for(int i=1;i<s1.size();i++){
+            bool op1=isScramble(s1.substr(0,i),s2.substr(0,i)) && isScramble(s1.substr(i,s1.size()-i),s2.substr(i,s1.size()-i));
+            bool op2=isScramble(s1.substr(0,i),s2.substr(s1.size()-i,i)) && isScramble(s1.substr(i,s1.size()-i),s2.substr(0,s1.size()-i));
+            if(op1 || op2){
+                flag=true;
+                break;
+            }
+        }    
+        return mp[key]=flag;
     }
 };
