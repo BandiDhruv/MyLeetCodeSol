d6dc0c5ee1ee5815cfa189d44220c641b1ce848d
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-08-05 16:28:18 +0530

    Time: 581 ms (5.04%) | Memory: 25.1 MB (32.43%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        // vector<int> suf(s.size(),0),pre(s.size(),0);
-        int acount=0,bcount=0;
-        // for(int i=s.size()-2;i>=0;i--){
-        //     suf[i]=suf[i+1];
-        //     if(s[i+1]=='a')
-        //         suf[i]++;
-        // }
-        // int ans=suf[0]+pre[0];
-        int ans=INT_MAX;
-        for(int i=0;i<s.size();i++){
-            //  pre[i]=pre[i-1];
-            // if(s[i-1]=='b')pre[i]++;
-            // ans=min(ans,suf[i]+pre[i]);
-            if(s[i]=='a')acount++;
-        }
-        if(s.size()==acount)return 0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='a')acount--;
-            ans=min(ans,acount+bcount);
-            if(s[i]=='b')bcount++;
-            // else acount--;
+        stack<char> st;
+        int ans=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='a')st.push('a');
+            else {
+                if(st.empty())continue;
+                else if(st.top()=='a'){
+                    cout<<i<<"  ";
+                    st.pop();
+                    ans++;
+                }
+            }
         }
         return ans;
     }
