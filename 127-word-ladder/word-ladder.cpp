1e7c07cf21035328abd383c1064e7b18038289f8
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-08-06 15:57:58 +0530

    Time: 2219 ms (5.02%) | Memory: 40.6 MB (9.95%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,41 +2,30 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         queue<pair<string,int>> q;
-        q.push({beginWord,1});
-        // unordered_map<string,int> vis;
-        // for(int i=0;i<wordList.size();i++)
-        // {
-        //     vis[wordList[i]]=0;
-        // }
-        // int ans=INT_MAX;
-        unordered_set<string> st(wordList.begin(),wordList.end());
-        while(!q.empty())
-        {
-            string temp=q.front().first;
-            int l=q.front().second;
-            // vis[temp]=1;
-            st.erase(temp);
-            q.pop();
-            if(temp==endWord)
-                return l;
-            for(int i=0;i<temp.size();i++)
-            {
-                string h=temp;
-                for(char j='a';j<='z';j++)
-                {
-                    if(temp[i]==j)continue;
-                    temp[i]=j;
-                    // cout<<temp[i]<<"  "<<temp<<"   ";
-                    if(st.find(temp)!=st.end() /*&& vis[temp]!=1*/)
-                    {
-                        // cout<<temp<<"  ";
-                        q.push({temp,l+1});
-                    }
-                    temp=h;
-                }
-                // cout<<endl;
-            }
+        unordered_set<string> st;
+        for(auto it:wordList){
+            st.insert(it);
         }
+        q.push({beginWord,1});
+        set<string> vis;
+        while(!q.empty()){
+            auto temp=q.front().first;
+            int cnt=q.front().second;
+            vis.insert(temp);
+            q.pop();
+            if(temp==endWord){
+                return cnt;
+            }
+            for(int i=0;i<temp.size();i++){
+                string ori=temp;
+                for(char ch='a';ch<='z';ch++){
+                    ori[i]=ch;
+                    if(st.find(ori)!=st.end() && vis.find(ori)==vis.end()){
+                        q.push({ori,cnt+1});
+                    }
+                }
+            }
+        }    
         return 0;
     }
 };
