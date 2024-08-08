2717f51a284f4cd512e7a861f46d15606ea318dc
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-08-08 23:33:23 +0530

    Time: 101 ms (84.88%) | Memory: 84.5 MB (37.02%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    vector<int> nextSE(vector<int>&heights)
-    {
+    vector<int> pse(vector<int>&h){
         stack<int> st;
-        vector<int> ans(heights.size());
-        for(int i=heights.size()-1;i>=0;i--)
-        {
-            while(!st.empty() && heights[st.top()]>=heights[i])st.pop();
+        vector<int> ans(h.size());
+        for(int i=0;i<h.size();i++){
+            while(!st.empty() && h[st.top()]>=h[i])st.pop();
             if(st.empty())ans[i]=-1;
             else ans[i]=st.top();
             st.push(i);
         }
         return ans;
     }
-      vector<int> prevSE(vector<int>&heights)
-    {
+    vector<int> nse(vector<int>&h){
         stack<int> st;
-        vector<int> ans(heights.size());
-        for(int i=0;i<heights.size();i++)
-        {
-            while(!st.empty() && heights[st.top()]>=heights[i])st.pop();
-            if(st.empty())ans[i]=-1;
+        vector<int> ans(h.size());
+        for(int i=h.size()-1;i>=0;i--){
+            while(!st.empty() && h[st.top()]>=h[i])st.pop();
+            if(st.empty())ans[i]=h.size();
             else ans[i]=st.top();
             st.push(i);
         }
         return ans;
     }
-    int largestRectangleArea(vector<int>& heights) {
-        vector<int> nse=nextSE(heights);
-        vector<int> pse=prevSE(heights);
+    int largestRectangleArea(vector<int>& h) {
+        int l=0,r=h.size()-1;
+        vector<int> p=pse(h);
+        vector<int> n=nse(h);
         int ans=INT_MIN;
-        for(int i=0;i<heights.size();i++)
-        {
-            int nidx=nse[i];
-            int pidx=pse[i];
-            if(nidx==-1)
-                nidx=heights.size();
-            ans=max(ans,(nidx-pidx-1)*heights[i]);
+        for(int i=0;i<h.size();i++){
+            // cout<<h[i]<<" "<<n[i]<<" "<<p[i]<<endl;
+            ans=max(ans,h[i]*(n[i]-p[i]-1));
         }
         return ans;
     }
