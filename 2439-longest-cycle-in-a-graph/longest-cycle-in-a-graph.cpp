8312fb541ff625a1e3e59f1f24d494d0589856c8
Dhruv Bandi <110834730+BandiDhruv@users.noreply.github.com>
2024-08-15 14:48:30 +0530

    Time: 710 ms (47.28%) | Memory: 135.6 MB (48.88%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    int result=-1;
-    void dfs(int node,vector<int>&vis,
-    vector<int>& edges,vector<int>& count,
-    vector<int>& dfsvis)
-    {
-        if(node!=-1)
-        {
-            vis[node]=1;
-            dfsvis[node]=1;
-            int adjnode=edges[node];
-            if(adjnode!=-1 && !vis[adjnode])
-            {
-                count[adjnode]=count[node]+1;
-                dfs(adjnode,vis,edges,count,dfsvis);
+    int res = -1;
+    void dfs(int node, vector<int>& edges, vector<bool> &visited, vector<int>& dist, vector<bool> &pathvis){
+        if(node != -1) {
+            visited[node] = true;
+            pathvis[node] = true;
+            int adjnode= edges[node];
+            if(adjnode!=-1 && !visited[adjnode]) {
+                dist[adjnode] = dist[node] + 1;
+                dfs(adjnode, edges, visited, dist, pathvis);
+            } else if(adjnode!=-1 && pathvis[adjnode] == true) { 
+                res=max(res,dist[node]-dist[adjnode]+1);
             }
-            else{
-                if(adjnode!=-1 && dfsvis[adjnode])
-                {
-                    result = max(result,count[node]-count[adjnode]+1);
-                }
-            }
-            dfsvis[node]=0;
+            pathvis[node] = false;
         }
     }
+    
     int longestCycle(vector<int>& edges) {
-        int n=edges.size();
-        vector<int> vis(n,0),count(n,1);
-        vector<int> dfsvis(n,0);
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                dfs(i,vis,edges,count,dfsvis);
-            }
+        int n = edges.size();
+        vector<bool> visited(n, false);
+        vector<int> dist(n, 1);
+        vector<bool> pathvis(n, false);
+        for(int i = 0 ; i<n; i++) {
+            if(!visited[i]) {
+                dfs(i, edges, visited, dist, pathvis);
+            }   
         }
-        return result;
-
+        return res;
     }
 };
